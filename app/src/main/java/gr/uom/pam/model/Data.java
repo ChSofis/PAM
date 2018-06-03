57b954957ae34be158d4e9d448c5cacce7cd28ac
ChSofis <38702048+ChSofis@users.noreply.github.com>
2018-06-04 00:50:04 +0300

    Add files via upload

--- app/src/main/java/gr/uom/pam/model/Data.java
+++ app/src/main/java/gr/uom/pam/model/Data.java
@@ -7,36 +7,37 @@ public class Data {
     public static List<Category> GetCategories(){
         List<Category> categories = new ArrayList<>();
         categories.add(new Category("MULTI", "MULTI"));
-        categories.add(new Category("NDG", "NDG"));
-        categories.add(new Category("NESTLE PROFESSIONAL", "NESTLE PROFESSIONAL"));
-        categories.add(new Category("ΔΗΜΗΤΡΙΑΚΑ", "ΔΗΜΗΤΡΙΑΚΑ"));
-        categories.add(new Category("ΕΛΛΗΝΙΚΟΣ ΚΑΦΕΣ", "ΕΛΛΗΝΙΚΟΣ ΚΑΦΕΣ"));
-        categories.add(new Category("ΖΩΟΤΡΟΦΕΣ", "ΖΩΟΤΡΟΦΕΣ"));
-        categories.add(new Category("ΜΑΓΕΙΡΙΚΑ", "ΜΑΓΕΙΡΙΚΑ"));
-        categories.add(new Category("ΝΕΡΑ", "ΝΕΡΑ"));
-        categories.add(new Category("ΠΑΙΔΙΚΕΣ ΤΡΟΦΕΣ", "ΠΑΙΔΙΚΕΣ ΤΡΟΦΕΣ"));
-        categories.add(new Category("ΡΟΦΗΜΑΤΑ", "ΡΟΦΗΜΑΤΑ"));
-        categories.add(new Category("ΣΟΚΟΛΑΤΕΣ", "ΣΟΚΟΛΑΤΕΣ"));
-        categories.add(new Category("ΣΤΙΓΜΙΑΙΟΙ ΚΑΦΕΔΕΣ", "ΣΤΙΓΜΙΑΙΟΙ ΚΑΦΕΔΕΣ"));
+        categories.add(new Category("Μαγειρικά", "ΜΑΓΕΙΡΙΚΑ"));
+        categories.add(new Category("Σοκολάτα", "ΣΟΚΟΛΑΤΕΣ"));
+        categories.add(new Category("CPW", "ΔΗΜΗΤΡΙΑΚΑ"));
+        categories.add(new Category("Purina", "ΖΩΟΤΡΟΦΕΣ"));
+        categories.add(new Category("Nestle Waters", "ΝΕΡΑ"));
+        categories.add(new Category("Ελληνικός Καφές", "ΕΛΛΗΝΙΚΟΣ ΚΑΦΕΣ"));
+        categories.add(new Category("Στιγμιαίος Καφές", "ΣΤΙΓΜΙΑΙΟΙ ΚΑΦΕΔΕΣ"));
+        categories.add(new Category("Nestle Professional - OOH", "NESTLE PROFESSIONAL"));
+        categories.add(new Category("Dolce Gusto", "NDG"));
+        categories.add(new Category("Ροφήματα", "ΡΟΦΗΜΑΤΑ"));
+        categories.add(new Category("Παιδική Διατροφή", "ΠΑΙΔΙΚΕΣ ΤΡΟΦΕΣ"));
         return categories;
     }
 
     public static List<Store> GetStores() {
         List<Store> stores = new ArrayList<>();
-        stores.add(new Store("ΑΒ", "AB"));
-        stores.add(new Store("MARKET IN", "BGs\\MARKET IN"));
-        stores.add(new Store("ΑΝΕΔΗΚ ΚΡΗΤΙΚΟΣ", "ELOMAS\\ΑΝΕΔΗΚ ΚΡΗΤΙΚΟΣ"));
-        stores.add(new Store("ΠΡΟΜΗΘΕΥΤΙΚΗ)", "ELOMAS\\ΠΡΟΜΗΘΕΥΤΙΚΗ"));
-        stores.add(new Store("ΕΝΑ C&C", "ΕΝΑ C&C"));
-        stores.add(new Store("LIDL", "LIDL"));
-        stores.add(new Store("MAKRO", "MART)"));
-        stores.add(new Store("MASOUTIS", "MASOUTIS"));
-        stores.add(new Store("MASOUTIS C&C", "MASOUTIS C&C"));
-        stores.add(new Store("METRO C&C", "METRO C&C"));
-        stores.add(new Store("MY MARKET", "MY MARKET"));
+        stores.add(new Store("ΑΒ Βασιλόπουλος", "AB"));
+        stores.add(new Store("E.Y.Σ.", "SKLAVENITIS"));
+        stores.add(new Store("My Market", "MY MARKET"));
         stores.add(new Store("Γαλαξίας", "PENTE"));
-        stores.add(new Store("E.Y.Σ.", "SKLAVENITIS "));
-        stores.add(new Store("OK STORES", "OK STORES"));
+        stores.add(new Store("Market In", "BGs/1.MARKET IN"));
+        stores.add(new Store("ΑΝΕΔΗΚ Κρητικός", "ELOMAS/1. ΑΝΕΔΗΚ ΚΡΗΤΙΚΟΣ") );
+        stores.add(new Store("ΠΡΟΜΗΘΕΥΤΙΚΗ", "ELOMAS/3. ΠΡΟΜΗΘΕΥΤΙΚΗ"));
+        stores.add(new Store("Μασούτης", "MASOUTIS"));
+        stores.add(new Store("Ok Anytime Stores", "ΟΚ STORES"));
+        stores.add(new Store("LIDL", "LIDL"));
+        stores.add(new Store("ΕΝΑ C&C", "ENA C&C"));
+        stores.add(new Store("MART", "MAKRO)"));
+        stores.add(new Store("Μασούτης C&C", "MASOUTIS C&C"));
+        stores.add(new Store("METRO C&C", "METRO C&C"));
+
         return stores;
     }
 
